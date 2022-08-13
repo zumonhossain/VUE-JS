038a0b81c369a25dce1f1e077e61bfdcf7045a0c
zumonhossain <zumon15-13169@diu.edu.bd>
2022-08-13 19:13:09 +0600

    custome-search

---
+++ 16_custome-search/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <Navbar></Navbar>
+
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Post</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+   
+  </div>
+  
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  name: 'app',
+  data(){
+    return {
+      title: 'Hurray Our vue apps are running',
+     
+    }
+  },
+  components:{
+    Navbar
+   
+  },
+  methods:{
+  
+    
+  }
+}
+</script>
+
+<style>
+h2 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
