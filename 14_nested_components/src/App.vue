dcd461bb15eb4ea02e105be3e68638a9cc6edcc5
zumonhossain <zumon15-13169@diu.edu.bd>
2022-08-13 12:18:58 +0600

    nested-components

---
+++ 14_nested_components/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <Navbar></Navbar>
+
+   
+  </div>
+  
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  name: 'app',
+  data(){
+    return {
+      title: 'Hurray Our vue apps are running',
+     
+    }
+  },
+  components:{
+    Navbar
+   
+  },
+  methods:{
+  
+    
+  }
+}
+</script>
+
+<style>
+h2 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
